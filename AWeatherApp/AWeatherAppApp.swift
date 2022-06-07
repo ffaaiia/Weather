ec35f2edbcad2e4aa6025e6f6232ba8190858217
Fay Anas Alshiban <fayanasalshiban@Fays-MacBook-Pro.local>
2022-06-08 00:21:53 +0300

    first commit

---
+++ AWeatherApp/AWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWeatherAppApp.swift
+//  AWeatherApp
+//
+//  Created by Fay Anas Alshiban on 09/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct AWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWeatherApp
+//
+//  Created by Fay Anas Alshiban on 09/11/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
