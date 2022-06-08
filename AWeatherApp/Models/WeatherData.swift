2381f0440043a01c980e8bd69926b1f604755605
Fay Anas Alshiban <fayanasalshiban@Fays-MacBook-Pro.local>
2022-06-08 09:58:30 +0300

    Adding Models/Manger for the app

---
+++ AWeatherApp/Models/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  AWeatherApp
+//
+//  Created by Fay Anas Alshiban on 09/11/1443 AH.
+//
+
+import Foundation
---
+++ AWeatherApp/Models/WeatherModelView.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModelView.swift
+//  AWeatherApp
+//
+//  Created by Fay Anas Alshiban on 09/11/1443 AH.
+//
+
+import Foundation
+//View Model
